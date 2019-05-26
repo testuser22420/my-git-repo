84742ef4ab284302c5e97c5b03894ae18c141df4
tim schmidt <tps.orig@gmail.com>
2019-05-26 15:02:17 -0700

    added t2.py in master

---
+++ t2.py
@@ -0,0 +1,10 @@
+fname = "John"
+lname = "Doe"
+age = 24
+
+
+print "{} {} is {} years old".format(fname, lname, age)
+
+print '%-20s %s is %d years old' % (fname, lname, age)
+
+print '%-20s' % ("HI THERE")
